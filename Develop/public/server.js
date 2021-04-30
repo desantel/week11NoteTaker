bd3981596c74263d9fe753a8b85cc21063a0d8f5
Elizabeth De Santis <ecdesantis21@gmail.com>
2021-04-30 18:02:34 -0400

    initial commit

---
+++ Develop/public/server.js
@@ -0,0 +1,20 @@
+//dependencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+//PORT and express
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//parses data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require('/routes/routes')(app);
+
+//listener
+app.listen(PORT, function() {
+    console.log(`Listening on PORT: ${PORT}`);
+});
